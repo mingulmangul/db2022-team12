ea1a4aaf880d495e24844098f18826dbe0219635
Min A <min2010011@gmail.com>
2022-05-31 22:36:55 +0900

    Add MainFrame class

--- db2022-team12/src/MainClass.java
+++ db2022-team12/src/MainClass.java
@@ -1,27 +1,9 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		
-		String baseUrl = "jdbc:mysql://localhost:3306/";
-		String dbName = "test";
-		String username = "root";
-		String password = "1234";
-		
-		try (
-			Connection conn = DriverManager.getConnection(baseUrl + dbName, username, password);
-			Statement stmt = conn.createStatement();
-		){
-			System.out.println("Database is connected!!");
-			stmt.executeUpdate("INSERT sample VALUES (3);");
-			System.out.println("success");
-		} catch (SQLException sqle) {
-			System.out.println("Error >> " + sqle);
-		}
+		MainFrame frame = new MainFrame();
 		
 	}
 
---
+++ db2022-team12/src/MainFrame.java
@@ -0,0 +1,21 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	public MainFrame() {
+		setTitle("프로그램");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(300, 300);
+
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new FlowLayout());
+
+//		AuthenticationPanel authPanel = new AuthenticationPanel();
+//
+//		contentPane.add(authPanel);
+
+		setVisible(true);
+	}
+}
