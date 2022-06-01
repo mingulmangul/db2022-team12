f9c0021a216e1d660787618915e77999dfcf30f0
Min A <min2010011@gmail.com>
2022-06-02 01:06:19 +0900

    로그인 기능 구현

--- db2022-team12/src/AuthenticationPanel.java
+++ db2022-team12/src/AuthenticationPanel.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.regex.Pattern;
 
@@ -13,22 +14,31 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+// 로그인 및 회원가입
 public class AuthenticationPanel extends JPanel {
 
 	private JDialog signUpDialog;
+	private JPanel authPanel;
 	private JButton signInBtn, signUpBtn, submitBtn;
 	private JTextField idField, nameField, phoneField, emailField, addressField;
 	private JPasswordField pwField;
 	private JLabel noticeLabel, idLabel, pwLabel, nameLabel, phoneLabel, emailLabel, addressLabel;
 
-	private boolean isValid;
-
+	// 유저 정보 검색 쿼리
+	private static String SIGNIN_QUERY = "SELECT pw FROM member WHERE id = ?";
+	
+	// 유저 정보 삽입 쿼리
 	// id, pw, name, phone, email, address
 	private static String SIGNUP_QUERY = "INSERT INTO member VALUES (?, ?, ?, ?, ?, ?)";
 
 	public AuthenticationPanel() {
+		authPanel = new JPanel();
+		JPanel signInPanel = new JPanel(new GridLayout(2, 1));
 		JPanel signInTextFieldPanel = new JPanel(new GridLayout(2, 3));
 
+		noticeLabel = new JLabel();
+		signInPanel.add(noticeLabel);
+		
 		idLabel = new JLabel("ID: ");
 		idField = new JTextField();
 		pwLabel = new JLabel("Password: ");
@@ -38,26 +48,65 @@ public class AuthenticationPanel extends JPanel {
 		signInTextFieldPanel.add(idField);
 		signInTextFieldPanel.add(pwLabel);
 		signInTextFieldPanel.add(pwField);
-
+		signInPanel.add(signInTextFieldPanel);
+		
 		signInBtn = new JButton("로그인");
 		signInBtn.addActionListener(new signInBtnListener());
 		signUpBtn = new JButton("회원가입");
 		signUpBtn.addActionListener(new signUpBtnListener());
 
-		this.add(signInTextFieldPanel);
-		this.add(signInBtn);
-		this.add(signUpBtn);
+		authPanel.add(signInPanel);
+		authPanel.add(signInBtn);
+		authPanel.add(signUpBtn);
+		this.add(authPanel);
 	}
 
+	// <로그인> 버튼에 대한 리스너
+	// DB에 사용자로부터 받은 입력과 일치하는 데이터가 존재하는지 확인 후 로그인
 	private class signInBtnListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-
+			String id = idField.getText().strip();
+			String pw = String.valueOf(pwField.getPassword()).strip();
+			
+			// 사용자 입력 유효성 검증
+			if (id.isEmpty()) {
+				noticeLabel.setText("아이디를 입력해주세요");
+			} else if (pw.isEmpty()) {
+				noticeLabel.setText("비밀번호를 입력해주세요");
+			} else {
+				
+				// 모든 필드가 유효하다면 데이터베이스 연결
+				try (Connection conn = new ConnectionClass().getConnection();
+						PreparedStatement preStmt = conn.prepareStatement(SIGNIN_QUERY);) {
+					// 유저 정보 검색을 위한 쿼리
+					preStmt.setString(1, id);
+					ResultSet res = preStmt.executeQuery();
+					
+					// 해당 아이디의 유저가 존재하는지 확인
+					if (!res.next()) {
+						noticeLabel.setText("존재하지 않는 아이디입니다");
+					} else {
+						// 비밀번호 검증
+						String stored_pw = res.getString("pw");
+						if (!pw.equals(stored_pw)) {
+							noticeLabel.setText("잘못된 비밀번호입니다");
+						} else {
+							// 로그인 성공
+							authPanel.setVisible(false);
+						}
+					}
+				} catch (SQLException sqle) {
+					System.out.println(sqle);
+				}
+			}
 		}
 
 	}
 
+	// <회원가입> 버튼에 대한 리스너
+	// 회원가입 Dialog 창 생성
 	private class signUpBtnListener implements ActionListener {
 
 		@Override
@@ -110,13 +159,17 @@ public class AuthenticationPanel extends JPanel {
 
 	}
 
+	// <가입하기> 버튼 리스너
+	// 사용자로부터 받은 입력으로 쿼리 생성 후 DB에 삽입
 	private class submitBtnListener implements ActionListener {
 
+		// 유효한 전화번호인지 검증
 		private boolean isValidPhone(String phone) {
 			String phonePattern = "^\\d{2,3}-\\d{3,4}-\\d{4}$";
 			return Pattern.matches(phonePattern, phone);
 		}
 
+		// 유효한 이메일 주소인지 검증
 		private boolean isValidEmail(String email) {
 			String emailPattern = "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}$";
 			return Pattern.matches(emailPattern, email);
@@ -131,6 +184,7 @@ public class AuthenticationPanel extends JPanel {
 			String email = emailField.getText().strip();
 			String address = addressField.getText().strip();
 
+			// 사용자가 입력한 각 필드의 내용에 대한 유효성 검증
 			if (id.isEmpty()) {
 				noticeLabel.setText("아이디를 입력해주세요");
 			} else if (pw.isEmpty()) {
@@ -143,8 +197,10 @@ public class AuthenticationPanel extends JPanel {
 				noticeLabel.setText("잘못된 이메일 주소입니다");
 			} else {
 
+				// 모든 필드가 유효하다면 데이터베이스 연결
 				try (Connection conn = new ConnectionClass().getConnection();
 						PreparedStatement preStmt = conn.prepareStatement(SIGNUP_QUERY);) {
+					// Member INSERT를 위한 쿼리 생성 및 커밋
 					preStmt.setString(1, id);
 					preStmt.setString(2, pw);
 					preStmt.setString(3, name);
@@ -154,12 +210,14 @@ public class AuthenticationPanel extends JPanel {
 					preStmt.executeUpdate();
 				} catch (SQLException sqle) {
 					int error = sqle.getErrorCode();
+					// ID(Primary Key) 중복 에러 처리
 					if (error == ConnectionClass.ER_DUP_ENTRY || error == ConnectionClass.ER_DUP_KEY)
 						noticeLabel.setText("중복된 아이디입니다");
 					
 					System.out.println(sqle);
 				}
 				
+				// 회원가입 dialog 닫기
 				signUpDialog.dispose();
 			}
 		}
