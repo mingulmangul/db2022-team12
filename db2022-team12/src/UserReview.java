94b9a80c4847f214e809a25273b5aa47d1db270d
Hyeon0630 <89723637+Hyeon0630@users.noreply.github.com>
2022-06-04 16:49:23 +0900

    충돌 방지

--- db2022-team12/src/UserReview.java
+++
@@ -1,9 +0,0 @@
-
-public class UserReview {
-	// ID, Musical_id, Rate, Written_at
-	public static int review_id;
-	public static int musical_id;
-	public static String musical_name;
-	public static int rate;
-	public static String written_at;
-}
