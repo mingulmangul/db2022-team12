53d83579102083127a7b7711edf8680045981664
Hyeon0630 <89723637+Hyeon0630@users.noreply.github.com>
2022-06-04 16:49:17 +0900

    충돌 방지

--- db2022-team12/src/UserTicket.java
+++
@@ -1,9 +0,0 @@
-
-public class UserTicket {
-	//Musical_id, Member_id, Theater_name, Order_date
-	public static int musical_id;
-	public static String musical_name;
-	public static String theater;
-	public static String orderDate; 
-	
-}
