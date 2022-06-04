7b0187309224257d114c99fded8a60eae556d1e0
Hyeon0630 <89723637+Hyeon0630@users.noreply.github.com>
2022-06-04 16:29:09 +0900

    충돌 방지

--- db2022-team12/src/ConnectionClass.java
+++ db2022-team12/src/ConnectionClass.java
@@ -1,8 +1,9 @@
+package DB2022Team12;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionClass {
+class ConnectionClass {
 	
 	private String baseUrl = "jdbc:mysql://localhost:3306/";
 	private String dbName = "DB2022Team12";
