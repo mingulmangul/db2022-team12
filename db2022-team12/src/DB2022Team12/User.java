d81553681ab4fd17ff883b32a712a009a58574c1
Min A <min2010011@gmail.com>
2022-06-04 20:41:31 +0900

    User class getter/setter 추가

--- db2022-team12/src/DB2022Team12/User.java
+++ db2022-team12/src/DB2022Team12/User.java
@@ -1,6 +1,19 @@
 package DB2022Team12;
 
 class User {
-	public static String ID;
-	public static String NAME;
+	private static String id = null;
+	private static String name = null;
+	
+	static String getId() {
+		return id;
+	}
+	static String getName() {
+		return name;
+	}
+	static void setId(String id) {
+		User.id = id;
+	}
+	static void setName(String name) {
+		User.name = name;
+	}
 }
