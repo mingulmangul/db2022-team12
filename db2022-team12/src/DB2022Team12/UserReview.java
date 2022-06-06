207c8db524c1f452d1b3ea3ccd742a7a420c580f
Hyeon0630 <sonabykwon@gmail.com>
2022-06-06 14:01:26 +0900

    수정본2

--- db2022-team12/src/DB2022Team12/UserReview.java
+++ db2022-team12/src/DB2022Team12/UserReview.java
@@ -1,7 +1,7 @@
 package DB2022Team12;
 
 /**
- * 사용자 리뷰 정보 저장
+ * 사용자 리뷰 정보 저장 클래스
  * 
  * @author sonab
  *
@@ -14,56 +14,72 @@ class UserReview {
 	String time;
 
 	/**
-	 * getter & setter
+	 * 사용자 리뷰의 리뷰 식별자를 리턴하는 메소드
+	 * 
+	 * @return 리뷰 식별자
 	 */
 	int getID() {
 		return ID;
 	}
 
 	/**
-	 * getter & setter
+	 * 객체 생성 시 사용자 리뷰의 리뷰 식별자를 저장하는 메소드
+	 * 
+	 * @param iD 리뷰 식별자
 	 */
 	void setID(int iD) {
 		ID = iD;
 	}
 
 	/**
-	 * getter & setter
+	 * 사용자 리뷰의 공연 제목을 리턴하는 메소드
+	 * 
+	 * @return 리뷰 공연 제목
 	 */
 	String getTitle() {
 		return title;
 	}
 
 	/**
-	 * getter & setter
+	 * 객체 생성 시 사용자 리뷰의 공연 제목을 저장하는 메소드
+	 * 
+	 * @param title 공연 제목 
 	 */
 	void setTitle(String title) {
 		this.title = title;
 	}
 
 	/**
-	 * getter & setter
+	 * 사용자 리뷰의 평점을 리턴하는 메소드
+	 * 
+	 * @return 리뷰의 평점
 	 */
 	int getRate() {
 		return rate;
 	}
 
 	/**
-	 * getter & setter
+	 * 객체 생성 시 사용자 리뷰의 평점을 저장하는 메소드
+	 * 
+	 * @param rate 평점
 	 */
 	void setRate(int rate) {
 		this.rate = rate;
 	}
 
 	/**
-	 * getter & setter
+	 * 사용자 리뷰의 리뷰 작성 날짜를 리턴하는 메소드
+	 * 
+	 * @return 리뷰 작성 날짜
 	 */
 	String getTime() {
 		return time;
 	}
 
 	/**
-	 * getter & setter
+	 * 객체 생성 시 사용자 리뷰의 리뷰 작성 날짜를 저장하는 메소드
+	 * 
+	 * @param time 리뷰 작성 날짜
 	 */
 	void setTime(String time) {
 		this.time = time;
--- db2022-team12/src/DB2022Team12/UserTicket.java
+++ db2022-team12/src/DB2022Team12/UserTicket.java
@@ -13,42 +13,54 @@ class UserTicket {
 	String orderDate;
 
 	/**
-	 * getter & setter
+	 * 사용자 티켓의 예매 티켓 식별자를 리턴하는 메소드
+	 * 
+	 * @return 티켓의 예매 티켓 식별자
 	 */
 	int getID() {
 		return ID;
 	}
 
 	/**
-	 * getter & setter
+	 * 객체 생성 시 사용자 티켓의 예매 티켓 식별자를 저장하는 메소드
+	 * 
+	 * @param iD 예매 티켓 식별자
 	 */
 	void setID(int iD) {
 		ID = iD;
 	}
 
 	/**
-	 * getter & setter
+	 * 사용자 티켓의 공연 제목을 리턴하는 메소드
+	 * 
+	 * @return 티켓의 공연 제목
 	 */
 	String getTitle() {
 		return title;
 	}
 
 	/**
-	 * getter & setter
+	 * 객체 생성 시 사용자 티켓의 공연 제목을 저장하는 메소드
+	 * 
+	 * @param title 티켓 공연 제목
 	 */
 	void setTitle(String title) {
 		this.title = title;
 	}
 
 	/**
-	 * getter & setter
+	 * 사용자 티켓의 티켓 예매 날짜를 리턴하는 메소드
+	 * 
+	 * @return 티켓 예매 날짜
 	 */
 	String getOrderDate() {
 		return orderDate;
 	}
 
 	/**
-	 * getter & setter
+	 * 객체 생성 시 사용자 티켓의 티켓 예매 날짜를 저장하는 메소드
+	 * 
+	 * @param orderDate 티켓 예매 날짜
 	 */
 	void setOrderDate(String orderDate) {
 		this.orderDate = orderDate;
